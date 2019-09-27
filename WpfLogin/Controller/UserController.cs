ddc9a092392b7df256eb6af7be8a7fb2d5310826
RamaPras <rama96pras@gmail.com>
2019-09-27 17:06:39 +0700

    Add project files.

---
+++ WpfLogin/Controller/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfLogin.Context;
+
+namespace WpfLogin.Controller
+{
+    public class UserController
+    {
+        public static void AddUser()
+        {
+            using (var ObjContext = new MyContext())
+            {
+                var User = new User
+                {
+                    userName = "Joshua",
+                    Address = "Tanjung Duren"
+                };
+                ObjContext.Teachers.Add(Teacher);
+                ObjContext.SaveChanges();
+            }
+        }
+    }
+}
